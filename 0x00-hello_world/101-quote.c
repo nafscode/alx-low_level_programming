58f8fff49aeca7bff219441e3ba9e337277c3e20
nafscode <ahmadnafs@gmail.com>
2023-04-20 02:27:37 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,8 +7,8 @@
 
 int main(void)
 {
-	fprintf(stderr, "and that piece of
-			art is useful\"
-			- Dora Korpar, 2015-10-19\n");
+	char q[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, q, strlen(q));
 	return (1);
 }
