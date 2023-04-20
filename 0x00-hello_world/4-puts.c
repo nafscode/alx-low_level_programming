ebfd71012ef588265941caabc59095bb7bcf6ac8
nafscode <ahmadnafs@gmail.com>
2023-04-20 01:01:27 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * Main - Print a line using puts
+ * Return: 0
+ */
+
+int main(void)
+{
+	char str[] = "\"Programming is like building a multilingual puzzle";
+	puts(str);
+	return 0;
+}
