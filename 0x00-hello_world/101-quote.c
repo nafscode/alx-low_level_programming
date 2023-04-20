e6ab6fae9f2ab3eccccd06e9e17b6b0d49dcba1d
nafscode <ahmadnafs@gmail.com>
2023-04-20 02:29:57 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - Print to standart error
@@ -9,6 +10,6 @@ int main(void)
 {
 	char q[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(2, q, strlen(q));
+	write(2, q, 59);
 	return (1);
 }
