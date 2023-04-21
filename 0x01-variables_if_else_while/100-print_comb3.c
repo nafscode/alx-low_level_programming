64eff3f91cce7b8b803517a028aa7dda576c2e8f
nafscode <ahmadnafs@gmail.com>
2023-04-21 05:14:26 -0700

    print comb

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Write a program that prints
+ * all possible different combinations
+ * of two digits.
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 48; i < 58; i++)
+	{
+		for (j = i + 1; j < 10; j++)
+		{
+			putchar(ij);
+		}
+	}
+	return (0);
+}
