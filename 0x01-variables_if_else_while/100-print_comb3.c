6c8991da4cb108e02fde50c56b35986405fc624e
nafscode <ahmadnafs@gmail.com>
2023-04-21 05:28:22 -0700

    print comb

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,13 +12,14 @@ int main(void)
 	int i;
 	int j;
 
-	for (i = 48; i < 58; i++)
+	for (i = 48; i < 57; i++)
 	{
 		for (j = i + 1; j < 58; j++)
 		{
 			putchar(i);
-			putchar(44);
 			putchar(j);
+			putchar(44);
+			putchar(32);
 		}
 	}
 	return (0);
