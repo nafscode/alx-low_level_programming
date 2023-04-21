148e371455416014f05766fc4fa44d2b393eb149
nafscode <ahmadnafs@gmail.com>
2023-04-21 08:01:00 -0700

    print comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -21,7 +21,7 @@ int main(void)
 			putchar(32);
 			putchar((j / 10) + 48);
 			putchar((j % 10) + 48);
-			if (( i == 98) && (j == 99))
+			if ((i == 98) && (j == 99))
 				continue;
 			putchar(44);
 			putchar(32);
