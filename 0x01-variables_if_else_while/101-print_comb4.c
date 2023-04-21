9b0ef71d649e3752bd58623d0e841280ba85dc65
nafscode <ahmadnafs@gmail.com>
2023-04-21 06:00:13 -0700

    print comb4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - Writ a program that prints all possible
+ * different combinations of three digits
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = 48; i < 56; i++)
+	{
+		for (j = i + 1; j < 57; j++)
+		{
+			for (k = j + 1; k < 58; k++)
+			{
+				putchar(i);
+				putchar(j);
+				putchar(k);
+
+				if ((i == 55) && (j == 56) && (k == 57))
+					continue;
+
+				putchar(44);
+				putchar(32);
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
