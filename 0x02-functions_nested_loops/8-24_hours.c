af2e55a95d994e074b6bb003db5e9d0a7827d21b
nafscode <ahmadnafs@gmail.com>
2023-04-25 12:00:18 +0000

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
-/** 
- * main - A function that prints every minute of the
+/**
+ * jack_bauer - A function that prints every minute of the
  * day of Jack Bauer, starting from 00:00 to 23:59.
  * Return: 0
  */
@@ -15,11 +15,11 @@ void jack_bauer(void)
 	{
 		for (j = 0; j < 60; j++)
 		{
-			_putchar(i / 10);
-			_putchar(i % 10);
+			_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
 			_putchar(':');
-			_putchar(j / 10);
-			_putchar(j % 10);
+			_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
 			_putchar('\n');
 		}
 	}
