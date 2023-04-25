265f7092c2ed389710981dcf224ba6e3228d4f2c
nafscode <ahmadnafs@gmail.com>
2023-04-25 11:11:18 +0000

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * Write a function that prints the
+ * print_alphabet - Write a function that prints the
  * alphabet, in lowercase, followed by a new line.
  * Return: 0
  */
