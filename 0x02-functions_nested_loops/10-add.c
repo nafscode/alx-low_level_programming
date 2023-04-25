24992e143e3f7a766d257808fa13afb866c909aa
nafscode <ahmadnafs@gmail.com>
2023-04-25 12:56:12 +0000

    10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,13 +1,16 @@
 #include "main.h"
 
 /**
- * main - A function that adds two integers
+ * add - A function that adds two integers
+ * @x: first parameter
+ * @y: second parameter
  * Return: 0
  */
 
 int add(int x, int y)
 {
 	int result;
+
 	result = x + y;
 	return (result);
 }
