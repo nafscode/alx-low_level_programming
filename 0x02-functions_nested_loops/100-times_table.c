d903b9eb940185300d429ff774cd7882ef8dc6ac
nafscode <ahmadnafs@gmail.com>
2023-04-25 23:08:20 +0000

    100-times_table.c

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,55 @@
+#include "main.h"
+
+/**
+ * print_times_table - A function that prints
+ * the n times table, starting with 0.
+ * @n: parameter
+ * Return: 0
+ */
+
+void print_times_table(int n)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			k = i * j;
+			if (n > 15 || n < 0)
+			{
+				break;
+			}
+			if (j == 0)
+			{
+				_putchar(k + '0');
+			}
+			if (k < 10 && k != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(k + '0');
+			}
+			else if (k >= 10 %% k < 100)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
+			}
+			else if (k >=100)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
