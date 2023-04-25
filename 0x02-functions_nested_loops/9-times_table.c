4846cdd8e8500d941ea07b8f08e36ce27b8e4c81
nafscode <ahmadnafs@gmail.com>
2023-04-25 12:43:18 +0000

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,18 +15,22 @@ void times_table(void)
 
 	for (i = 0; i <= n; i++)
 	{
-		for (j = 0; j <= 10; j++)
+		for (j = 0; j <= 9; j++)
 		{
 			k = i * j;
+			if (j == 0)
+			{
+				_putchar(k + '0');
+			}
 			
-			if (k < 10)
+			if (k < 10 && j != 0)
 			{
 				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
 				_putchar(k + '0');
 			}
-			else
+			else if (k >= 10
 			{
 				_putchar(',');
 				_putchar(' ');
