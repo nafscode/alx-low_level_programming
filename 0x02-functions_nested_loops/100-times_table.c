8eef5c34b1af3ad7817c7ef03d7ffa3ae84c17cf
nafscode <ahmadnafs@gmail.com>
2023-04-26 08:44:27 +0000

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -4,9 +4,7 @@
  * print_times_table - A function that prints
  * the n times table, starting with 0.
  * @n: parameter
- * Return: 0
  */
-
 void print_times_table(int n)
 {
 	int i, j, k;
@@ -18,7 +16,6 @@ void print_times_table(int n)
 			for (j = 0; j <= n; j++)
 			{
 				k = i * j;
-
 				if (j == 0)
 				{
 					_putchar(k + '0');
