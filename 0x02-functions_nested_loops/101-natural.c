3d11ff429176cf71d58fabe6f3273eca2ef90e5e
nafscode <ahmadnafs@gmail.com>
2023-04-26 09:17:56 +0000

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - print natural numbers
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+	int n = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3 = 0) && (i %% 5 = 0))
+		{
+			n = n + 1;
+		}
+	}
+	printf("%d\n", n)
+}
