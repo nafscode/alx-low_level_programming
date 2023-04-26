7f92d4001944a5bec5fe5c9b0e92115f9db7ecd4
nafscode <ahmadnafs@gmail.com>
2023-04-26 11:39:51 +0000

    103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - print sum of fibonacci sequence
+ * Return: 0
+ */
+
+int main(void)
+{
+	long int a = 1;
+	long int b = 2;
+	long int c = a + b;
+	long int limit;
+	int i;
+	long int sum;
+
+	for (i = 0; i <= limit; i++)
+	{
+		if (c % 2 == 0)
+		{
+			sum = sum + i;
+		}
+		else
+		{
+			continue;
+		}
+	}
+	printf("%lu\n", sum);
+	return (0);
+}
