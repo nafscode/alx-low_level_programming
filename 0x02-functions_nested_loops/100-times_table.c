bc6747fe1140a5e6b16b15bbc0231eadebe642af
nafscode <ahmadnafs@gmail.com>
2023-04-26 00:49:00 +0000

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,46 +9,41 @@
 
 void print_times_table(int n)
 {
-	int i;
-	int j;
-	int k;
-
-	for (i = 0; i <= n; i++)
+	int i, j, k;
+	if (n >= 0 && <= 15)
 	{
-		for (j = 0; j <= n; j++)
+		for (i = 0; i <= n; i++)
 		{
-			k = i * j;
-			if (n > 15 || n < 0)
-				break;
-			if (k > 99)
+			for (j = 0; j <= n; j++)
 			{
-				_putchar(k / 100 + '0');
-				_putchar((k / 10 % 10) + '0');
-				_putchar(k % 10 + '0');
+				k = i * j;
+				if (k == 0)
+				{
+					_putchar(k + '0');
+				}else if (k < 10 && k != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(k + '0');
+				}else if (k >= 10 && k > 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((k / 10) + '0');
+					_putchar((k % 1)0 + '0');
+				}else if (k >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((k / 100) + '0');
+					_putchar(((k / 10) % 10) + '0');
+					_putchar((k % 10) + '0');
+				}
 			}
-			if (k > 9)
-			{
-				_putchar(' ');
-				_putchar(k / 10 + '0');
-				_putchar(k % 10 + '0');
-			}
-			else if (k <= 9 || k != 0)
-			{
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(k + '0');
-			}
-			else
-			{
-				_putchar(k + '0');
-			}
-			if (j != 0)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
+			_putchar('\n');
 		}
-		_putchar('\n');
-
 	}
 }
