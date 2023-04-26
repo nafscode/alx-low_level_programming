e0e1f412bb506a7d98ebc168f85195c893ce65c4
nafscode <ahmadnafs@gmail.com>
2023-04-26 08:41:02 +0000

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -23,7 +23,7 @@ void print_times_table(int n)
 				{
 					_putchar(k + '0');
 				}
-				else if (k < 10 && k != 0)
+				else if (k < 10 && j != 0)
 				{
 					_putchar(',');
 					_putchar(' ');
