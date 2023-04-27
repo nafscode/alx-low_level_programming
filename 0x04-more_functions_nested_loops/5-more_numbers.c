3ce7cf259d52ba1162eb3a20bafb2e23e3de3561
nafscode <ahmadnafs@gmail.com>
2023-04-27 12:05:42 +0000

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -3,7 +3,6 @@
 /**
  * more_numbers - A function that prints 10 times the numbers, from 0 to 14,
  * followed by a new line.
- * Return: 0
  */
 
 void more_numbers(void)
@@ -17,7 +16,7 @@ void more_numbers(void)
 			if (j >= 10)
 			{
 				_putchar(j / 10 + '0');
-			_putchar(j % 10 + '0');
+				_putchar(j % 10 + '0');
 			}
 		}
 		_putchar('\n');
