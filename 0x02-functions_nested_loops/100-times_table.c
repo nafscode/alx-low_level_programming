03fa2be0f7205527ed28a8701b43b2b49dbb22da
nafscode <ahmadnafs@gmail.com>
2023-04-27 03:21:50 +0000

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,37 +11,30 @@ void print_times_table(int n)
 	{
 		for (i = 0; i <= n; i++)
 		{
-			for (j = 0; j <= n; j++)
+			_putchar('0');
+			for (j = 1; j <= n; j++)
 			{
+				_putchar(',');
+				_putchar(' ');
 				k = i * j;
-				if (j == 0)
+				if (k <= 99)
 				{
-					_putchar(k + '0');
+					_putchar(' ');
 				}
-				else if (k < 10 && j != 0)
+				if (k <= 9)
 				{
-					_putchar(',');
 					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(k + '0');
 				}
-				else if (k >= 10 && k < 100)
+				if (k > 99)
 				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar((k / 10) + '0');
-					_putchar((k % 10) + '0');
-				}
-				else if (k >= 100)
-				{
-					_putchar(',');
-					_putchar(' ');
 					_putchar((k / 100) + '0');
-					_putchar(((k / 10) % 10) + '0');
-					_putchar((k % 10) + '0');
+					_putchar(((k /10) % 10) + '0');
 				}
+				else if (k <= 99 && k > 9)
+				{
+					_putchar((k / 10) + '0');
+				}
+					_putchar((k % 10) + '0');
 			}
 			_putchar('\n');
 		}
