36264afce22d4328f6273647ed60976200466875
nafscode <ahmadnafs@gmail.com>
2023-04-28 13:59:10 +0000

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,5 +1,6 @@
 #include "main.h"
 
+
 /**
  * print_number - A function that prints an integer.
  * @n: parameter
@@ -7,5 +8,14 @@
 
 void print_number(int n)
 {
-	_putchar(n / 10 + '0');
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n / 10)
+	{
+		print_number(n / 10);
+	}
+	_putchar(n % 10 + '0');
 }
