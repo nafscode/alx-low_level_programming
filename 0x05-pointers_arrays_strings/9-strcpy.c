b86707aac3be18a3663f2fde46dbbcba72ff02e9
nafscode <ahmadnafs@gmail.com>
2023-05-02 23:26:53 +0000

    9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strcpy - a function that copies the string pointed to by src,
+ * including the terminating null byte (\0), to the buffer pointed to by dest.
+ * @dest: parameter
+ * @src: parameter
+ * Return: dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+	int j;
+
+	j = strlen(src);
+	for (i = 0; i < j; i++)
+	{
+		dest[i] = src[i]
+	}
+	return (*dest);
+}
