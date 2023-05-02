6e7fd98a57b6db836c21dcfb500024c2bc06a6da
nafscode <ahmadnafs@gmail.com>
2023-05-02 17:24:06 +0000

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - A function that swaps the values of two integers.
+ * @a: 1st parameter
+ * @b: 2nd parameter
+ * Return: nothing
+ */
+
+void swap_int(int *a, int *b)
+{
+	int c;
+	c = *a;
+	*a = *b;
+	*b = c;
+}
