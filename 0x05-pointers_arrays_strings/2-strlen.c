18e1b572ee92e3d675da4178a4b2e75c68d7d80f
nafscode <ahmadnafs@gmail.com>
2023-05-02 17:47:28 +0000

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen - A function that returns the lenght of a string.
+ * @s: parameter
+ * Return: nothing
+ */
+
+int _strlen(char *s)
+{
+	int len;
+
+	len = 0;
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	return (len);
+}
