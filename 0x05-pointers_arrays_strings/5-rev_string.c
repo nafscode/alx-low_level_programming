559a1a8d2d297b61b753a4332cfc893052dfef37
nafscode <ahmadnafs@gmail.com>
2023-05-02 20:11:56 +0000

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,18 +9,17 @@
 
 void rev_string(char *s)
 {
-	int len;
 	int i;
 	int j;
+	char temp;
 
-	len = strlen(s);
-	for (j = 0; s[j] != '\0'; j++)
+	j = strlen(s);
+	for (i = 0; i < j - 1; i++)
 	{
-		_putchar(s[j]);
+		char temp = s[i];
+		s[i] = s[j];
+		s[j] = temp;
+		j--;
 	}
-	_putchar('\n');
-	for (i = len - 1; i >= 0; i--)
-	{
-		_putchar(s[i]);
-	}
+
 }
