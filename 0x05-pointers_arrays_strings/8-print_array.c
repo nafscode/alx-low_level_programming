f3158e989939dd5e88fec86892e1ac418be6c275
nafscode <ahmadnafs@gmail.com>
2023-05-02 23:12:03 +0000

    8-print_array.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,25 @@
+#include <string.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_array - A function that prints n elements of an array of integers,
+ * followed by a new line.
+ * @a: 1st parameter
+ * @n: 2nd parameter
+ * Return: nothing
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+	for (i = 0; i < n; i++)
+	{
+		printf("%i", a[i]);
+		if i != n - 1)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}
