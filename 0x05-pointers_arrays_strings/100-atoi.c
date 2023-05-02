9e627406c503e414d2b8a59e74c006ae0f1ec8b3
nafscode <ahmadnafs@gmail.com>
2023-05-02 23:53:34 +0000

    100-atoi.c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _atoi - A function that convert a string to an integer.
+ * @s: parameter
+ * Return: an int
+ */
+
+int _atoi(char *s)
+{
+	int i;
+	int j;
+
+	j = strlen(s);
+	for (i = 0; i < j; i++)
+	{
+		if s[i] >= 0 || s[i] <= 9
+		{
+			_putchar(s[i] + '0');
+		}
+		else
+		{
+			return;
+		}
+	}
+}
