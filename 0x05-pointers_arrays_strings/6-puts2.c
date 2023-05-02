6a7945b6fd813578afa2ed207adc59b924ec792b
nafscode <ahmadnafs@gmail.com>
2023-05-02 21:15:58 +0000

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,15 +11,9 @@ void puts2(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] != '\0'; i++)
+	for (i = 0; str[i] != '\0'; i +=2)
 	{
-		if (i % 2 == 0)
-		{
-			_putchar(str[i]);
-		}
-		else
-		{
-			return;
-		}
+	
+		_putchar(str[i]);
 	}
 }
