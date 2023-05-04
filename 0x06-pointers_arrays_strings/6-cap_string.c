6fe56fe20ab1e4a5f98033b8bc1034d4f027b2a5
nafscode <ahmadnafs@gmail.com>
2023-05-04 14:51:10 +0000

    6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <string.h>
+#include <ctype.h>
+
+/**
+ * cap_string - A function that capitalizes all words of a string.
+ * @str: string
+ * Return: char
+ */
+
+char *cap_string(char *str)
+{
+	int i;
+	int j;
+
+	str[0] = toupper(str[0]);
+
+	for (i = 1; i < strlen(str); i++)
+	{
+		for (j = 0; j < 13; i++)
+		{
+			if (str[i] == sep[i])
+			{
+				str[i] = toupper(str[i]);
+			}
+		}
+	}
+	return (str);
+}
+
+
