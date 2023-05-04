29bde401f2ce18755dbedd1268902180c84f1cd7
nafscode <ahmadnafs@gmail.com>
2023-05-04 20:51:48 +0000

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -27,7 +27,7 @@ char *cap_string(char *src)
 			{
 				if ((*(src + (i + 1)) >= 97) && (*(src + (i + 1)) <= 122))
 					*(src + (i + 1)) = *(s + (i + 1)) - 32;
-				break
+				break;
 			}
 		}
 		i++;
