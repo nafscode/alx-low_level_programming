f22ae38f83609facf0532a0548d1f49f35b02646
nafscode <ahmadnafs@gmail.com>
2023-05-04 12:47:31 +0000

    4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -11,15 +11,12 @@
 void reverse_array(int *a, int n)
 {
 	int i;
+	int temp;
 
 	for (i = 0; i < n / 2; i++)
 	{
 		temp = a[i];
 		a[i] = a[n - i - 1];
 		a[n - i - 1] = temp;
-	}
-	for (i = 0; i < n; i++)
-	{
-		_putchar(a + '0');
 	}
 }
