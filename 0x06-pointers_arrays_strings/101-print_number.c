fa4eb0841436790e44ea70b165ffbf2d5961dc58
nafscode <ahmadnafs@gmail.com>
2023-05-04 21:33:33 +0000

    101-print_number.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_number - A function that prints an integer
+ * @n: paameter
+ */
+
+void print_number(int n)
+{
+	unsigned int n1;
+
+	if (n < 0)
+	{
+		x = -n;
+		_putchar('-');
+	}
+	else
+	{
+		x = n;
+	}
+	if (x / 10)
+		print_number(x / 10);
+
+	_putchar((x % 10) + '0');
+}
