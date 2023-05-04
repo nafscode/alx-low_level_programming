7fe483419abb327a8ba5043dfcc796cadade7505
nafscode <ahmadnafs@gmail.com>
2023-05-04 21:11:49 +0000

    100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string using rot13
+ * @src: string
+ * Return: string
+ */
+
+char *rot13(char *src)
+{
+	int i;
+	int j;
+
+	char alpha[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char rot13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		for (j = 0; alpha[j] != '\0' j++)
+		{
+			if (src[i] == alpha[j])
+			{
+				src[i] = rot13[j];
+				break;
+			}
+		}
+	}
+	return (src);
+}
