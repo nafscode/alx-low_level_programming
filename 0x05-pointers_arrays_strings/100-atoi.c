fecaf5b47b38b13aa081abee862d088709bc43af
nafscode <ahmadnafs@gmail.com>
2023-05-05 17:42:44 +0000

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,22 +10,28 @@ int _atoi(char *s)
 {
 	int sign = 1;
 	unsigned int digit = 0;
+	int i = 0;
 
-	do {
-		if (*s == '-')
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-')
 		{
 			sign *= -1;
 		}
-		else if (*s >= '0' && *s <= '9')
+		if (s[i] == '-' || s[i] == '+')
 		{
-			digit = (digit * 10) + (*s - '0');
+			i++;
+			continue;
 		}
-		else if (digit > 0)
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = digit * 10 + (s[i] - '0');
+		}
+		else
 		{
 			break;
 		}
-	} while (*s++);
-
+		i++;
+	}
 	return (digit * sign);
 }
-
