b007830d643fcae72f746efb74020b045b7fed83
nafscode <ahmadnafs@gmail.com>
2023-05-08 12:35:07 +0000

    4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -10,17 +10,17 @@
 char *_strpbrk(char *s, char *accept)
 {
 	int i;
+	int j;
 
-	while (*s != '\0')
+	for (i = 0; *(s + i) != '\0'; i++)
 	{
-		for (i = 0; accept[i] != '\0'; i++)
+		for (j = 0; *(accept + j) != '\0'; i++)
 		{
-			if (*s == accept[i])
+			if (*(s + i) == *(accept + j))
 			{
-				return (s);
+				return (s + i);
 			}
 		}
-		s++;
 	}
 	return (NULL);
 }
