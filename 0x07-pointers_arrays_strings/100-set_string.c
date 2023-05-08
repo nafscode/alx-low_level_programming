62bf36d873c8330ea667d9225e8dbe750d5e5ad5
nafscode <ahmadnafs@gmail.com>
2023-05-08 15:04:54 +0000

    100-set_string.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - A function that sets the value of a pointer to a char.
+ * @s: pointer
+ * @to: character
+ * Return: Nothing
+ */
+
+void set_string(char **s, char *to)
+{
+	int *s = to;
+}
