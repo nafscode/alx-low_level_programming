5c365fdb0d4afc87c43f02097ba68b366301111b
nafscode <ahmadnafs@gmail.com>
2023-05-08 13:25:22 +0000

    8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -10,4 +10,14 @@
 
 void print_diagsums(int *a, int size)
 {
+	int sum1 = 0;
+	int sum2 = 0;
+	int i;
 
+	for (i = 0; i < size; i++)
+	{
+		sum1 += *(a + i * size + i);
+		sum2 += *(a + i * size + size - i -1);
+	}
+	printf("%d, %d", sum1. sum2);
+}
