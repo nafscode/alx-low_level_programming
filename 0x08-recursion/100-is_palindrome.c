a613ed863be0299a5833e15fc2590f6d88db2564
nafscode <ahmadnafs@gmail.com>
2023-05-10 14:03:15 +0000

    100-is_palindrome.c

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * reverse - A function that reverse a string
+ * @s: parameter
+ */
+
+void reverse(char *s)
+{
+	if (*s)
+	{
+		reverse(s + 1);
+		_putchar(*s);
+	}
+}
+
+/**
+ * print - A function that prints a string
+ * @s: parameter
+ */
+
+void print(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		print(s + 1);
+	}
+}
+/**
+ * is_palindrome - A function that returns 1 if a string is a palindrome
+ * and 0 if not.
+ * @s: string parameter
+ * Return: 1 or 0
+ */
+
+int is_palindrome(char *s)
+{
+	if (*s = '\0')
+		return (1);
+	if (print(*s) == reverse(*s))
+		return (1);
+	else
+		return (0);
+}
