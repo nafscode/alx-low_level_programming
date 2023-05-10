80d33194e794245834c2695ae21bbfb70bed01ff
nafscode <ahmadnafs@gmail.com>
2023-05-10 08:44:51 +0000

    readme

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - A function that prints a string, followed by a new line.
+ * @s: parameter
+ */
+
+void _puts_recursion(char *s)
+{
+	if ((*s >= 'a' || *s >= 'A') && (*s <= 'z' || *s <= 'Z'))
+	{
+		_putchar(*s +_puts_recursion(*s + 1));
+	}
+	_putchar('\n');
+}
