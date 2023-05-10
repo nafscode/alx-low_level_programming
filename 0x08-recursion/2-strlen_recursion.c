45f7d62c96ddbadff6a3be40bd07e44c1816129f
nafscode <ahmadnafs@gmail.com>
2023-05-10 10:31:05 +0000

    2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - A function that returns the lenght of a string.
+ * @s: parameter
+ */
+
+int _strlen_recursion(char *s)
+{
+	int count = 0;
+
+	if (*s)
+	{
+		count++;
+		_strlen_recursion(s + 1);
+	}
+	return (count);
+}
