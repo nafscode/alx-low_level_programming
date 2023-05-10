1edc91e4e26092a0264023c57e9770604ef2f5a5
nafscode <ahmadnafs@gmail.com>
2023-05-10 08:51:28 +0000

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,9 +7,13 @@
 
 void _puts_recursion(char *s)
 {
-	if ((*s >= 'a' || *s >= 'A') && (*s <= 'z' || *s <= 'Z'))
+	int i = 0;
+
+	while (*s != '\0')
 	{
-		_putchar(*s +_puts_recursion(*s + 1));
+		_putchar(*s +_puts_recursion(s + 1));
+		i++;
+		s++;
 	}
 	_putchar('\n');
 }
