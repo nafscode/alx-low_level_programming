4cc89594f32c5cb9ea8836cd4123c83785fa8e1e
nafscode <ahmadnafs@gmail.com>
2023-05-10 12:05:29 +0000

    5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - A function that returns the natural square root of a number
+ * @n: number
+ * Return: square root
+ */
+
+int _sqrt_recursion(int n)
+{
+	int another_sqrt(int n, int x);
+
+	x = 1;
+
+	return (another_sqrt(n, x));
+}
+
+/**
+ * another_sqrt - Another function to solve first function
+ * @x: number to check
+ * @n: parameter from first function
+ * Return: Square root
+ */
+
+int another_sqrt(int n, int x)
+{
+	if (x * x == n)
+	{
+		return (x);
+	}
+	else if (x < n)
+	{
+		return (another_sqrt(n, x + 1));
+	}
+	else
+	{
+		return (-1);
+	}
+}
