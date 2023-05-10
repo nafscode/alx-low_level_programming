f11c850a735eda5af556bb5871cc380f186672aa
nafscode <ahmadnafs@gmail.com>
2023-05-10 12:39:00 +0000

    6-is_prime_number.c

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * is_prime_number - A function that returns 1 if the input integer is a
+ * prime number, otherwise return 0
+ * @n: parameter
+ * Return: 1 or 0
+ */
+
+int is_prime_number(int n)
+{
+	int prime(int n, int k)
+
+	if (n == 2)
+	{
+		return (1);
+	}
+	return (prime(n, 2))
+}
+
+/**
+ * prime - check if a number is prime
+ * @n: number
+ * @k: parameter to check
+ * Return: int
+ */
+
+int prime(int n, int k)
+{
+	if (n < 2 || n % k == 0)
+	{
+		return (0);
+	}
+	else if (k > n / 2)
+	{
+		return (1);
+	}
+	else {
+		return (prime(k + 1, n));
+	}
+}
