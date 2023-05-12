d99f013a1b5ec379947250af90856582771986f4
nafscode <ahmadnafs@gmail.com>
2023-05-12 11:50:37 +0000

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - A program that prints its name, followed by a new line.
+ * Return: 0
+ */
+
+int main(int argc, char *argv)
+{
+	printf("argv\n", argv);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+# argc & arg v 
