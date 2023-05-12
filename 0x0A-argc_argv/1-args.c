0c05b4fb40126350a3c1185620134ead73a73e2d
nafscode <ahmadnafs@gmail.com>
2023-05-12 12:29:18 +0000

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints the number of arguments passed into it.
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
