b5b37e0ed3cbd325d56b155447144be750fd7248
nafscode <ahmadnafs@gmail.com>
2023-05-12 12:49:34 +0000

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - A program that multiplies two numbers
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 or Error, followed by a new line and then 1
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	int j;
+	int k;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	
+	i = atoi(argv[1]);
+	j = atoi(argv[2]);
+	k = i * j;
+
+	printf("%d\n", k);
+	return (0);
+}
+	
