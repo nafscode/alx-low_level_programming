5ab4c6ae610bc698f0a3bb82670003261fde34ed
nafscode <ahmadnafs@gmail.com>
2023-05-12 13:15:43 +0000

    4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - A program that adds positive numbers
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 or Error followed by a new line, then 1
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j]; j++)
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+	
+		k += atoi(argv[i]);
+		
+	}
+	printf("%d\n", k);
+	return (0);
+}
