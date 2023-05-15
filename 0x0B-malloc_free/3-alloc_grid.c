e4da32c96f484f2c46df8b3eacec246c77c8f14c
nafscode <ahmadnafs@gmail.com>
2023-05-15 18:12:57 +0000

    3-alloc_grid.c

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * alloc_grid - A function that returns a pointer to a 2 dimensional
+ * array of integers.
+ * @width: parameter
+ * @height: parameter
+ * Return: pointer or NULL
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **arr[][];
+	int i;
+	int j;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	arr[][] = malloc(sizeof(int) * (width * height));
+
+	if (arr[][] == NULL)
+		return (NULL);
+
+	for (i = 0; i < width; i++)
+	{
+		for (j = 0; j < height; j++)
+		{
+			arr[i][j] = 0;
+		}
+	}
+
+	return (arr[][]);
+}
