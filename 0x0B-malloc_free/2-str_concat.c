dc59e0883ecdb5519b0cc21d715703dc2828c6c8
nafscode <ahmadnafs@gmail.com>
2023-05-15 14:02:51 +0000

    2-str_concat.c

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * str_concat - A function that concatenates two strings.
+ * @s1: first string
+ * @s2: second string
+ * Return: pointer or NULL
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *s3;
+
+	s3 = (char *) malloc(strlen(s1) + strlen(s2));
+
+	strcpy(s3, s1);
+
+	while (*s2 != '\0')
+		*s3++ = *s2++;
+	*z = '\0';
+	
+	return (s3);
+}
