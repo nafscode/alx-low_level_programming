bf5fde45b6da79fba842cf8c6d16d641048e9737
nafscode <ahmadnafs@gmail.com>
2023-05-15 14:31:04 +0000

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -17,13 +17,22 @@ char *str_concat(char *s1, char *s2)
 	unsigned int n = strlen(s1);
 	unsigned int m = strlen(s2);
 
-	s3 = (char *)malloc(n + m + 2);
+	if (s1 == NULL)
+		s1 = ' ';
+
+	if (s2 == NULL)
+		s2 = ' ';
+
+	s3 = (char *)malloc(n + m + 1) * sizeof(char);
 	if (s3 == NULL)
+	{
 		return (NULL);
-	
+	}
+
 	for (i = 0; i <= n; i++)
 		*s3++ = *s1++;
 	for (i = 0; i <= m; i++)
 		*s3++ = *s2++;
+
 	return (s3);
 }
