9ca668b0547034641af03366301f59c7ff42c544
nafscode <ahmadnafs@gmail.com>
2023-05-15 16:29:16 +0000

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -33,9 +33,10 @@ char *str_concat(char *s1, char *s2)
 
 	for (i = 0; i < n; i++)
 		s3[i] = s1[i];
-
-	for (j = 0; j <= m; j++)
-		s3[j] = s2[j];
+	
+	m = j;
+	for (j = 0; j <= m; i++, j++)
+		s3[i] = s2[j];
 
 	return (s3);
 }
