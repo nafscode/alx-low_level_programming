b5261f3d82ae146fbdb185de34ba758a58cef481
nafscode <ahmadnafs@gmail.com>
2023-05-16 07:37:36 +0000

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -20,28 +20,23 @@ int **alloc_grid(int width, int height)
 	{
 		return (NULL);
 	}
-	arr = (int **)malloc(sizeof(int) * height);
+	arr = malloc(sizeof(*arr) * height);
 	if (arr == NULL)
 	{
-		free(arr);
 		return (NULL);
 	}
-	for (i = 0; i < width; i++)
-	{
-		arr[i] = (int *)malloc(sizeof(int) * width);
-		if (arr[i] == NULL)
-		{
-			free(arr[i]);
-			return (NULL);
-		}
-	}
-
 	for (i = 0; i < height; i++)
 	{
-		for (j = 0; j < width; j++)
+		arr[i] = malloc(sizeof(**arr) * width);
+		if (arr[i] == NULL)
 		{
-			arr[i][j] = 0;
+			for (i--; i >= 0; i--)
+				free(arr[i]);
+			free(arr);
+			return (NULL);
 		}
+		for (j = 0; j < width; j++)
+			arr[i][j] = 0;
 	}
 
 	return (arr);
