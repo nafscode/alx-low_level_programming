111a77bd05d3fc1f130b5b1a09285d5c9f18fa65
nafscode <ahmadnafs@gmail.com>
2023-05-17 17:45:08 +0000

    100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -22,8 +22,7 @@ char *argstostr(int ac, char **av)
 	if (ac == 0 || av == NULL)
 		return (NULL);
 
-	lenght = 0;
-	for (i = 0; i < ac; i++)
+	for (i = 0; lenght = 0; i < ac; i++)
 	{
 		for (j = 0; *(*(av + i) + j) != '\0'; j++, lenght++)
 			;
