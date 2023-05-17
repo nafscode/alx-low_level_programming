62c1d692d05fa9b2a8e2927d6df0e50041078765
nafscode <ahmadnafs@gmail.com>
2023-05-17 17:33:49 +0000

    100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,11 +1,10 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 /**
  * argstostr - A function that concatenates all the arguments of your program
- * @ac: integer parameter
+ * @ac: argument count
  * @av: character parameter(pointer to an array)
  * Return: pointer to a new sring or NULL
  */
@@ -17,27 +16,34 @@ char *argstostr(int ac, char **av)
 		return (NULL);
 	}
 	
-	char *str_ptr;
-	int i, j, lenght, index;
+	char *str_ptr, *start;
+	int i, j, lenght;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
 
 	lenght = 0;
 	for (i = 0; i < ac; i++)
 	{
-		lenght += strlen(av[i]) + 1;
+		for (j = 0; *(*(av + i) + j) != '\0'; j++, lenght++)
+			;
+		lenght++;
 	}
+	lenght++;
 
-	str_ptr = (char *)malloc(sizeof(char) * lenght);
-	if (str_ptr == NULL)
+	start = malloc(lenght * sizeof(char));
+	if (start == NULL)
 		return (NULL);
-
-	index = 0;
-	for (j = 0; i < ac; j++)
-	{
-		strcpy(str_ptr + index, av[i]);
-		index += strlen(av[i]);
-		str_ptr[index++] = '\n';
-	}
-	str_ptr[index-1] = '\0';
-
+	
+	str_ptr = start;
+	for (i = 0; i < ac; i++)
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			start = av[i][j];
+			start++;
+		}
+		*start = '\n';
+		start++;
+	
 	return (str_ptr);
 }
