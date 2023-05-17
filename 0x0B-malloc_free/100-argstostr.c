de746d8e1fa336b9383e1a32311dc2aad318f7e5
nafscode <ahmadnafs@gmail.com>
2023-05-17 16:49:45 +0000

    100-argstostr.c

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * argstostr - A function that concatenates all the arguments of your program
+ * @ac: integer parameter
+ * @av: character parameter(pointer to an array)
+ * Return: pointer to a new sring or NULL
+ */
+
+char *argstostr(int ac, char **av)
+{
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
+	
+	char *str_ptr;
+	int i;
+
+	str_ptr = malloc(sizeof(*char) * ac);
+	
+	if (str_ptr == NULL)
+	{
+		return (NULL);
+	}
+	else
+		for (i = 0; i < ac; i++)
+		{
+			str_ptr[i] = av[i];
+			str_ptr += '\n';
+		}
+
+	return (str_ptr);
+}
+
