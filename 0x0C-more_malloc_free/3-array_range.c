c448a8237cdd67adc094a7533d8fe5e47d6d1b6c
nafscode <ahmadnafs@gmail.com>
2023-05-18 12:37:29 +0000

    3-array_range.c

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * array_range - A function that creates an array of integers
+ * @min: minimum value
+ * @max: maximum value(excluded)
+ * Return: pointer to the new array
+ */
+
+int *array_range(int min, int max)
+{
+	int *arr;
+	int num;
+
+	if (min > max)
+		return (NULL);
+
+	num = max - min + 1;
+
+	arr = malloc(sizeof(int) * num);
+	if (arr = NULL)
+		return (NULL);
+	for (i = 0; i < num; i++)
+		arr[i] = min + i;
+
+	return (arr);
+}
+
