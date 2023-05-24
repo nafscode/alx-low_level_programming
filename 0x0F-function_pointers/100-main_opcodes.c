0056572f83987b0b699ad2be69b251cfbaac4442
nafscode <ahmadnafs@gmail.com>
2023-05-24 15:32:20 +0000

    100-main_opcodes.c

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - A program har prints the opcodes of its own main function
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	int bytes;
+	char *ptr;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	bytes = atoi(argv[1]);
+
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		return 2;
+	}
+
+	ptr = (char *)main;
+
+	for (i = 0; i < bytes; i++)
+		printf("%02x ", ptr[i]);
+
+	printf("\n");
+	return (0);
+}
