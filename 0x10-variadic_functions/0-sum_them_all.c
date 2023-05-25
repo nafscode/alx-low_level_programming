85dec123dd1751c9e2b50e25e8915fcfb0894756
nafscode <ahmadnafs@gmail.com>
2023-05-25 12:28:24 +0000

    0-sum_them_all.c

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,31 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * sum_them_all - A function that returns the sum of all its parameters
+ * @n: numbers of arguments
+ * Return: sum or 0 if n is 0
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	int sum;
+	int count;
+	va_list args;
+
+	if (n == 0)
+		return (0);
+	sum = 0;
+
+	va_start(args, n);
+	
+	for (count = 0; count < n; count++)
+	{
+		sum += va_arg(args, int);
+	}
+	va_end(args);
+	return (sum);
+}
+
+
+
