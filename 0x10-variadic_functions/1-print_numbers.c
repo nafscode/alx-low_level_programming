0ab2654c48213e15118707f73560136238fe99fd
nafscode <ahmadnafs@gmail.com>
2023-05-25 12:44:11 +0000

    1-print_numbers.c

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,29 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * print_numbers - A function that prints numbers, followed by a new line
+ * @seperator: Seperator
+ * @n: numbers of arguments
+ * Return: nothing
+ */
+
+void print_numbers(const char *seperator, const unsigned int n, ...)
+{
+	unsigned int count;
+
+	va_list args;
+	va_start(args, n);
+
+	for (count = 0; count < n; i++)
+	{
+		printf("%d", va_arg(args, int));
+
+		if (seperator != NULL && i != n - 1)
+		{
+			printf("%s", seperator);
+		}
+	}
+	va_end(args);
+	printf("\n");
+}
