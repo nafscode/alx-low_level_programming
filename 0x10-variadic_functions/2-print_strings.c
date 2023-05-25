8b2dcf82004469fad6d4df4603f440d2dee2faf7
nafscode <ahmadnafs@gmail.com>
2023-05-25 13:40:33 +0000

    2-print_strings.c

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,39 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_strings - A function that prints strings, followed by a new line.
+ * @separator: Separator
+ * @n: numbers of arguments
+ * Return: nothing
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int count;
+	char *str
+
+	va_start(args, n);
+
+	for (count = 0; count < n; count++)
+	{
+		str = va_arg(arg, char *);
+
+		if (str == NULL)
+		{
+			printf("nil");
+		}
+		else
+		{
+			printf("%s", str);
+		}
+		if (separator != NULL && count != n - 1)
+		{
+			printf("%s", separator);
+		}
+	}
+	va_end(args);
+	printf("\n");
+}
