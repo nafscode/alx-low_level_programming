c515ac825cb127dbc570231acafa2d64d5b124db
nafscode <ahmadnafs@gmail.com>
2023-06-01 16:04:27 +0000

    added advanced task

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * before_main - A function that print words before the main function
+ * Return: nothing
+ */
+void before_main() __attribute__((constructor));
+
+void before_main()
+
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
