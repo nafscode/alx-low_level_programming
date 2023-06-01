a626d73c7bd5b8e9e0fbad277e4b609a2ccea3e2
nafscode <ahmadnafs@gmail.com>
2023-06-01 12:21:24 +0000

    added tast 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - A function that prints all the elements of a list_t list.
+ * @h: pointer to head of the list.
+ * Return: 0
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t lenght;
+
+	lenght = 0;
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[%d] %s\n", 0, "(nil)");
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		h = h->next;
+		lenght++;
+	}
+
+	return (lenght);
+}
