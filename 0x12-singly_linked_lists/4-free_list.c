ab26ab8d22be3b5508b93afea5f99445dc6e3ac4
nafscode <ahmadnafs@gmail.com>
2023-06-01 14:35:15 +0000

    added task 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_list - A function that frees a list_t list
+ * @head: Beginning of the list
+ * Return: nothing
+ */
+
+void free_list(list_t *head)
+{
+	list_t *current;
+
+	current = head;
+	
+	while (current != NULL)
+	{
+		next = current->next;
+		free(current);
+		current = next;
+	}
+}
