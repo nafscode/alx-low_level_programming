d05e13fb61905f57cfdc51af7b0caae7235cc4d2
nafscode <ahmadnafs@gmail.com>
2023-06-05 20:04:26 +0000

    2-add_nodeint.c

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * add_nodeint - A function that adds a new node to the beginning of a list
+ * @head: begining of the list
+ * @n: new node
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new_node;
+
+	new = malloc(sizeof(listint_t));
+
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->next = *head;
+	*head = new;
+
+	return (*head);
+}
