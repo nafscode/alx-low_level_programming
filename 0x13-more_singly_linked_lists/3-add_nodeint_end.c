d54cf04b89f5a2bb06c83dd48b7cc0ff8357a8e2
nafscode <ahmadnafs@gmail.com>
2023-06-05 21:18:18 +0000

    3-add_nodeint_end.c

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -7,7 +7,7 @@
  * Return: address of the new element or NULL
  */
 
-listint_t *add_nodeint_end(listint_t **head, const in n)
+listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 
 	listint_t new_node = malloc(sizeof(listint_t));
