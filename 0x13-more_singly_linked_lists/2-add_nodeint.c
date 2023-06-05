25c63b8af66b1192772164fb495bd505c01ded30
nafscode <ahmadnafs@gmail.com>
2023-06-05 20:41:20 +0000

    2-add_nodeint.c

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -16,13 +16,13 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	{
 		return (NULL);
 	}
-	if (*head == NULL)
+	else
 	{
-		*head = new_node;
+		new_node->n = n;
+		new_node->next = *head;
 	}
-
-	new_node->n = n;
-	new_node->next = *head;
+	if (*head != NULL)
+		new_node->next = *head;
 	*head = new_node;
 
 	return (*head);
