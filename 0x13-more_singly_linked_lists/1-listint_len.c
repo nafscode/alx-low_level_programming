7208c7a3e68c95ddc92ee84bfd09ca3a6005d3da
nafscode <ahmadnafs@gmail.com>
2023-06-05 19:54:31 +0000

    1-listint_len.c

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * listint_len - A function that returns the number of elements in a list
+ * @h: Linked list
+ * Return: number of elements
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t len = 0;
+
+	while (h)
+	{
+		len++;
+		h = h->next;
+	}
+
+	return (len);
+}
