d5f805e4f785f7504747303ef50cf3e4c534a216
nafscode <ahmadnafs@gmail.com>
2023-06-06 09:30:45 +0000

    5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,10 +9,12 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
-	temp = head;
-	while (temp->next->next != NULL)
-	{
-		temp = temp->next;
-	}
-	temp->next = NULL;
+	if (head != NULL)
+		temp = *head;
+		while (temp != NULL)
+		{
+			temp = temp->next;
+			free(temp);
+		}
+		*head = NULL;
 }
