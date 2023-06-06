bf8a08e15374ee72ce138cbc8e74eb401a91a376
nafscode <ahmadnafs@gmail.com>
2023-06-06 09:59:33 +0000

    6-pop_listint.c

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * pop_listint - A function that delete the head node of a list
+ * and returns the node's data
+ * @head: head node
+ * Return: node's data or 0
+ */
+
+int pop_listint(listint_t **head)
+{
+	if (*head == NULL)
+		return (0);
+
+	listint_t *current = *head;
+	int result = current->data;
+
+	(*head) = (*head)->next;
+	free (current);
+
+	return (result);
+}
