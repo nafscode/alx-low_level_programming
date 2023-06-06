685451954cb2a6581f7d7de6c7e64763bd0d02f4
nafscode <ahmadnafs@gmail.com>
2023-06-06 13:21:20 +0000

    9-insert_nodeint.c

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - A function that inserts a new node
+ * at a given position
+ * @head: list
+ * @idx: index
+ * @n: new data
+ * Return: address of new node
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	unsigned int count;
+	listint_t *new_node;
+	listint_t *current;
+
+	current = head;
+
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+
+	for (count = 0; count < idx && current != NULL; count++)
+	{
+		current = current->next;
+	}
+	new_node->next = current->next;
+	current->next = new_node;
+
+	return (new_node);
+}
+
