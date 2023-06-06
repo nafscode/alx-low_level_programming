b4c05de614134d7d85c4f6b8695ae41091e8401f
nafscode <ahmadnafs@gmail.com>
2023-06-06 12:31:40 +0000

    2-add_nodeint.c

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -4,6 +4,7 @@
  * add_nodeint - A function that adds a new node to the beginning of a list
  * @head: begining of the list
  * @n: new node
+ * Return: new node
  */
 
 listint_t *add_nodeint(listint_t **head, const int n)
