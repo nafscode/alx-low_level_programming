fff2c3af6cc272ae1cf237e005fd132d0c9148f9
nafscode <ahmadnafs@gmail.com>
2023-06-06 10:19:53 +0000

    7-get_nodeint.c

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - A function that returns the nth node of a list
+ * @head: beginning of the list
+ * @index: index
+ * Return: nth node
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int count;
+	listint_t *temp;
+
+	temp = head;
+	count = 0;
+	while (temp != NULL)
+	{
+		if (count == index)
+			return (current->n);
+		count++;
+		current = current->next;
+	}
