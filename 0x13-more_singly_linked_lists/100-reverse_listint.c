5438f0c2825dc6551b2eb1368497f1c14cc1eb30
nafscode <ahmadnafs@gmail.com>
2023-06-06 17:47:44 +0000

    100-reverse_listint.c

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -9,13 +9,12 @@
 listint_t *reverse_listint(listint_t **head)
 {
 	listint_t *prev = NULL;
-	listint_t *current = *head;
 	listint_t *next = NULL;
 
-	while (*current != NULL)
+	while (*head != NULL)
 	{
-		next = (*current)->next;
-		(*current)->next = prev;
+		next = (*head)->next;
+		(*head)->next = prev;
 		prev = *current;
 		*current = next;
 	}
