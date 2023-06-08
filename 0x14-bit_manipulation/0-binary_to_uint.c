af91fb320be2ed7ada9ac33c5cf91857f2d6b33d
nafscode <ahmadnafs@gmail.com>
2023-06-08 20:06:59 +0000

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,23 +9,21 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int result;
-	int lenght;
 	int i;
 
 	result = 0;
-	lenght = strlen(b);
 
 	if (b == NULL)
 		return (0);
 
-	for (i = 0; i < lenght; i++)
+	for (i = 0; b[i] != '\0'; i++)
 	{
 		if (b[i] != '0' && b[i] != '1')
 		{
 			return (0);
 		}
 	}
-	for (i = 0; i < lenght; i++)
+	for (i = 0; b[i] != '\0'; i++)
 	{
 		result <<= 1;
 		if (b[i] == '1')
