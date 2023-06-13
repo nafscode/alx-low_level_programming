85f2445956841469d00d0a4b0014762e07e889b0
nafscode <ahmadnafs@gmail.com>
2023-06-13 15:18:33 +0000

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,14 +24,18 @@ int create_file(const char *filename, char *text_content)
 	if (file_des == -1)
 		return (-1);
 
-	if (text_content != NULL)
+	if (!text_content)
+		text_content = "";
+
+	for (len = 0; text_content[len]; len++)
+		;
+
+	nwrite = write(file_des, text_content, len);
+
+	if (nwrite == -1)
 	{
-		nwrite = write(file_des, text_content, len);
-		if (nwrite == -1)
-		{
-			close(file_des);
-			return (-1);
-		}
+		close(file_des);
+		return (-1);
 	}
 
 	close(file_des);
