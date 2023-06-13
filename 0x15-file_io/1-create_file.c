15c4d58c1f03cd9b3ef49cf37f9322a565793ff3
nafscode <ahmadnafs@gmail.com>
2023-06-13 15:12:05 +0000

    1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * create_file - A function that creates a file
+ * @filename: File name
+ * @text_content: Content
+ * Return: 1 on success, -1 on failure (file can not be created,
+ * file can not be written, write “fails”, etc…)
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int file_des;
+	int nwrite;
+	int len;
+
+	len = strlen(text_content);
+
+	if (!filename)
+		return (-1);
+
+	file_des = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (file_des == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		nwrite = write(file_des, text_content, len);
+		if (nwrite == -1)
+		{
+			close(file_des);
+			return (-1);
+		}
+	}
+
+	close(file_des);
+	return (1);
+}
